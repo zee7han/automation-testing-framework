2433bae1547ce9e43850f611f4f22166160bfc65
Mohammad Zeeshan <zee7han@gmail.com>
2018-06-06 16:30:38 +0530

    Update the app configurations

---
+++ Appium/configure.js
@@ -0,0 +1,14 @@
+module.exports.desiredCapabilities = {
+  automationName: 'appium',
+  platformName: 'Android',
+  platformVersion: '6.0.1',
+  deviceName: 'MOTO G3',
+  context: 'NATIVE_APP',
+  udid: // your device udid,
+  appSetupPath: //path to your apk file,
+  appPackage: // your app package,
+  appActivity: // your app activity,
+  appWaitActivity: // your app activity,
+  appWaitPackage: // your app wait package,
+  app: // path to your app apk file,
+}
---
+++ Appium/serverConfig.js
@@ -0,0 +1,7 @@
+module.exports.remotes = {
+  local: {
+    hostname: "127.0.0.1",
+    port: 4723,
+    url: "http://127.0.0.1:4723/wd/hub"
+  }
+}
